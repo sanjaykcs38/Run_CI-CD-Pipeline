dae12dda1a15b667a5704979117e15140104210d
sanjaykcs38 <sanjayk.cs38@gmail.com>
2026-08-24 23:17:35 +0530

    Final4

--- Parant_module/Environments/Pre-Prod/main.tf
+++ Parant_module/Environments/Pre-Prod/main.tf
@@ -1,26 +1,26 @@
 
 module "resource_group" {
-  source = "../../../Modules/azurerm_resource_group"
+  source = "../../../Child_module/azurerm_resource_group"
   rgs    = var.rgs
 }
 module "virtual_network" {
   depends_on = [module.resource_group]
-  source     = "../..../../Modules/azurerm_Virtual_Network"
+  source     = "../..../../Child_module/azurerm_Virtual_Network"
   vnets      = var.vnets
 }
 module "subnets" {
   depends_on = [module.virtual_network]
-  source     = "../../../Modules/azurerm_subnet"
+  source     = "../../../Child_module/azurerm_subnet"
   subnets    = var.subnets
 }
 module "public_ip" {
   depends_on = [module.resource_group]
-  source     = "../../../Modules/azurerm_public_ip"
+  source     = "../../../Child_module/azurerm_public_ip"
   PIP        = var.PIP
 }
 module "virtual_machine" {
   depends_on = [module.subnets, module.public_ip]
-  source     = "../../../Modules/azurerm_virtual_machine_NIC"
+  source     = "../../../Child_module/azurerm_virtual_machine_NIC"
   NICS_VM    = var.NICS_VM
 }
 
