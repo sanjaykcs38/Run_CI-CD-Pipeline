2b71165ba183329de0769c49a6dd17907b24b644
sanjaykcs38 <sanjayk.cs38@gmail.com>
2026-08-24 20:22:16 +0530

    final2

--- Parant_module/Environments/Pre-Prod/provider.tf
+++ Parant_module/Environments/Pre-Prod/provider.tf
@@ -6,15 +6,15 @@ terraform {
       version = "4.76.0"
     }
   }
-# backend "azurerm" {
-#     tenant_id            = "2121638c-2247-44a7-8b82-697632b4b79a"
-#     resource_group_name  = "RG_Storage1"
-#     storage_account_name = "landingzonestatefile"
-#     container_name       = "landingstate" 
-#     key                  = "prod.terraform.tfstate" 
-#   }
+  # backend "azurerm" {
+  #     tenant_id            = "2121638c-2247-44a7-8b82-697632b4b79a"
+  #     resource_group_name  = "RG_Storage1"
+  #     storage_account_name = "landingzonestatefile"
+  #     container_name       = "landingstate" 
+  #     key                  = "prod.terraform.tfstate" 
+  #   }
 
- }
+}
 
 provider "azurerm" {
   features {}
